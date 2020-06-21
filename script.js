0ab3dd0a9b943b955c95f86b46ff2c7055ed4797
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-06-21 21:14:07 +0800

    requestAnimationFrame()

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let x = 0;
+
 function render(){
+    x += 10;
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
     ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 75);
+    ctx.fillRect(x, 0, 150, 75);
+    window.requestAnimationFrame(render);
 }
 
-window.onload = render
+window.requestAnimationFrame(render);
