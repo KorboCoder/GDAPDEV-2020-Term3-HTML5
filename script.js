19dae0540b32c6838f935b86691627a9bfd9df79
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-06-21 21:59:15 +0800

    keyboard input

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let x = 0;
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
 
 function render(){
     x += 1;
-    let canvas = document.getElementById("myCanvas");
-    let ctx = canvas.getContext("2d");
 
     //clear everything before we render
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -12,5 +12,17 @@ function render(){
     ctx.fillRect(x, 0, 150, 75);
     window.requestAnimationFrame(render);
 }
+
+
+function on_key_event(event){
+    console.log("test");
+}
+
+canvas.addEventListener("keyup", on_key_event);
+canvas.addEventListener( "keydown", on_key_event);
+
+// hack to focus on canvas object
+canvas.setAttribute('tabindex','0');
+canvas.focus();
 
 window.requestAnimationFrame(render);
