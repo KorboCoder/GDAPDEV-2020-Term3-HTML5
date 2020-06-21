943ba05893870a158108928fced769e7c80509d2
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-06-21 21:24:06 +0800

    clear frame

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 let x = 0;
 
 function render(){
-    x += 10;
+    x += 1;
     let canvas = document.getElementById("myCanvas");
     let ctx = canvas.getContext("2d");
+
+    //clear everything before we render
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(x, 0, 150, 75);
     window.requestAnimationFrame(render);
