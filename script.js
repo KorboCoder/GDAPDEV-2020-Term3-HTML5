907ba187972985736c2b925e5b6eaafe660f4625
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-07-03 22:17:56 +0800

    'easejs'

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
-let x = 0;
+
 let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
+let stage = new createjs.Stage("myCanvas");
+let x = 0;
+
+let rect = new createjs.Shape();
+rect.graphics.beginFill("#FF0000").drawRect(0,0,150,75);
+rect.x = 0;
+rect.y = 0;
+stage.addChild(rect);
+
+createjs.Ticker.addEventListener("tick", render);
+createjs.Ticker.framerate = 300;
 
 function render(){
     x += 1;
+    rect.x = x;
+    stage.update();
 
-    //clear everything before we render
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(x, 0, 150, 75);
-    window.requestAnimationFrame(render);
 }
-
 
 function on_key_event(event){
     console.log(event);
+}
+
+ // hack to focus on canvas object
+function focus_on_canvas(){
+    canvas.setAttribute('tabindex', '0');
+    canvas.focus();
 }
 
 canvas.addEventListener("keyup", on_key_event);
