2ea96652ece10900b58a4d3bf49da2267ec57e8e
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-07-09 23:23:32 +0800

    print out user agent

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 
-function init(){
-    document.getElementById('log_div').textContent = navigator.userAgent;
-    try{
-        let test = [{name: "Juan", age: 32}, {name: "Jose", age: 23}]
-        let resultIdx = test.findIndex(find_juan);
-        
-        document.getElementById('log_div').textContent = JSON.stringify(test[resultIdx]);
-    }
-    catch(err){
-        // work around goes here
-        document.getElementById('log_div').textContent = "Not Supported: " + err;
-    }
-}
-
-function find_juan(element){
-    return element.name == "Juan";
+function init(){  
+    document.getElementById('log_div').textContent = navigator.userAgent
 }
