0a35a36d9cf93f44da0bdb26de4e3b1e0b6672e2
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-07-10 00:32:00 +0800

    print out if on windows

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
 function init(){  
-    document.getElementById('log_div').textContent = navigator.userAgent
+
+    let is_windows =  /windows/i.test(navigator.userAgent)
+    document.getElementById('log_div').textContent = is_windows? "You're on windows" : "Not on Windows"
+    
 }
