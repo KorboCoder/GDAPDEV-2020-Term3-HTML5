b316c087853daac71b47872c67efc5dabc63a03a
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-08-02 19:35:49 +0800

    exact pixel positioning

---
+++ SampleGame/src/layouts/PercentPositioningLayout.js
@@ -0,0 +1,21 @@
+class PercentPositioningLayout extends ccui.Layout{
+    constructor(){
+        super();
+    }
+
+    onEnter(){
+        super.onEnter();
+        this.setContentSize(cc.winSize);
+
+        let text = new ccui.Text("Hello_World", "Pixel", 32);
+        // Exact pixel
+        //text.x = 300
+        //text.y = 200
+        text.setPositionType(ccui.Widget.POSITION_PERCENT)
+        text.setPositionPercent(cc.p(.30,.20) );
+
+        this.addChild(text);
+        this.scheduleUpdate();
+        this.addComponent(new FitToWindow());
+    }
+}
