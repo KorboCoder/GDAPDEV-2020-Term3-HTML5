0500ff0114c50dc635d9f3c411e46cb540258d0f
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-08-04 00:50:58 +0800

    refactoring

--- SampleGame/src/components/FitToParent.js
+++ SampleGame/src/components/FitToParent.js
@@ -1,44 +1,35 @@
-class FitToParent extends cc.Component{
+class FitToParent extends ResizeListener{
+    constructor(){
+        super();
+        this.setName("FitToParent");
+    }
     onEnter(){
         super.onEnter();
         cc.assert(this.getOwner() instanceof ccui.Widget, "Componnent compatible only with ccui.Widget")
-        this.listener = cc.EventListener.create({
-            event: cc.EventListener.CUSTOM,
-            eventName: 'canvas-resize',
-            callback: this.onResize.bind(this)
-        });
-        cc.eventManager.addListener(this.listener, this.getOwner());
-        this.isResizeContent = true;
     }
 
     onResize(){
-        this.isResizeContent = true;
-    }
-    
-    update(dt){
-        if(this.isResizeContent){
-            let owner = this.getOwner();
+        let owner = this.getOwner();
 
-            let ownerSize = owner.getContentSize();
-            let parentSize = owner.getParent().getContentSize();
+        let ownerSize = owner.getContentSize();
+        let parentSize = owner.getParent().getContentSize();
 
-            let scaleX = parentSize.width / ownerSize.width;
-            let scaleY = parentSize.height / ownerSize.height;
+        let scaleX = parentSize.width / ownerSize.width;
+        let scaleY = parentSize.height / ownerSize.height;
 
-            let targetScale = 1
-            if(scaleX < scaleY){
-                targetScale = scaleX;
-            }
-            else{
-                targetScale = scaleY;
-            }
-            
-            if(targetScale > 1){
-                targetScale = 1
-            }
-            owner.setScale(targetScale);
-            this.isResizeContent = false;
+        let targetScale = 1
+        if(scaleX < scaleY){
+            targetScale = scaleX;
         }
-    }
+        else{
+            targetScale = scaleY;
+        }
+        
+        if(targetScale > 1){
+            targetScale = 1
+        }
+        owner.setScale(targetScale);
+        this.isResizeContent = false;
+    }   
 
 }
--- SampleGame/src/components/FitToWindow.js
+++ SampleGame/src/components/FitToWindow.js
@@ -1,25 +1,14 @@
-class FitToWindow extends cc.Component{
+class FitToWindow extends ResizeListener{
+    constructor(){
+        super();
+        this.setName("FitToWindow");
+    }
     onEnter(){
-        super.onEnter();
         cc.assert(this.getOwner() instanceof ccui.Layout, "Componnent compatible only with ccui.Layout")
-        this.listener = cc.EventListener.create({
-            event: cc.EventListener.CUSTOM,
-            eventName: 'canvas-resize',
-            callback: this.onResize.bind(this)
-        });
-        cc.eventManager.addListener(this.listener, this.getOwner());
-        this.isResizeContent = false;
+        super.onEnter();
     }
 
     onResize(){
-        this.isResizeContent = true;
+        this.getOwner().setContentSize(cc.winSize);
     }
-    
-    update(dt){
-        if(this.isResizeContent){
-            this.getOwner().setContentSize(cc.winSize);
-            this.isResizeContent = false;
-        }
-    }
-
 }
