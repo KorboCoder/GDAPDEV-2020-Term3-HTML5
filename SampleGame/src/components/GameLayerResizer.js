1c7ef683ccec59e18d9c7a3704d7032e66d49ea4
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-08-04 19:23:55 +0800

    added margins

--- SampleGame/src/components/GameLayerResizer.js
+++ SampleGame/src/components/GameLayerResizer.js
@@ -1,6 +1,7 @@
 class GameLayerResizer extends ResizeListener{
     constructor(){
         super();
+        this.padding = 40;
         this.setName("GameLayerResizer");
     }
 
@@ -16,8 +17,8 @@ class GameLayerResizer extends ResizeListener{
         owner.x = cc.winSize.width/2 - owner.width/2;
         owner.y = cc.winSize.height/2 - owner.height/2;
 
-        let scaleX = (cc.winSize.width) / ownerSize.width;
-        let scaleY = (cc.winSize.height)/ ownerSize.height;
+        let scaleX = (cc.winSize.width - this.padding) / ownerSize.width;
+        let scaleY = (cc.winSize.height - this.padding)/ ownerSize.height;
 
         let targetScale = 1
         if(scaleX < scaleY){
