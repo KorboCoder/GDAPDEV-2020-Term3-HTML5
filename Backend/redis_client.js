2d37a1dc452ea9f600eb355a5830f7dfc9cdcd90
Daniel Vallesteros <dmvallesteros@gmail.com>
2020-08-24 07:28:44 +0800

    added heroku

--- Backend/redis_client.js
+++ Backend/redis_client.js
@@ -1,6 +1,6 @@
 var Promise = require("bluebird");
 const redis = Promise.promisifyAll(require("redis"));
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_URL);
 
 client.on("error", function(error) {
     console.error(error);
